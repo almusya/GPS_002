193dc243a98d02c5c494594457a0cbfc35e9e532
almusya <almusya16@gmail.com>
2019-12-14 21:05:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/gps/MapsActivity.kt
@@ -0,0 +1,35 @@
+package com.example.gps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val uteyeah = LatLng(-7.747033, 110.355398)
+        mMap.addMarker(MarkerOptions().position(uteyeah).title("Universitas Terdepan Yogyakarta"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(uteyeah, 17.0f))
+    }
+}
